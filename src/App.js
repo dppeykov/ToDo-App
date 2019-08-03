9af62b1c0edf3150749d0627e9838ec62345c02e
Damyan Peykov <dppeykov@gmail.com>
2019-08-03 18:38:43 +0200

    fixing styles to be responsive

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userName: "Damyan"
+    };
+  }
+
+  changeNameHandler = e => console.log(e.target.value);
+
   render() {
     return (
-      <div className="App">
-        <h1>Hello form the todo app</h1>
+      <div className="container-fluid bg-primary text-white text-center text-sm-left p-2">
+        <div className="row" style={{ margin: 0 }}>
+          <div className="col-12 col-sm-7 col-md-8 col-lg-9 col-xl-10">
+            <h4 className="ml-sm-5">{this.state.userName}'s To Do List</h4>
+          </div>
+          <div className="col-12 col-sm-5 col-md-4 col-lg-3 col-xl-2">
+            <input
+              className="mx-auto input-group-text input-group-sm"
+              style={{ width: "200px", margin: 0 }}
+              placeholder="Fill your name here"
+              onChange={this.changeNameHandler}
+            />
+          </div>
+        </div>
       </div>
     );
   }
